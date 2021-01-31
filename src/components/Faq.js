a85af6ad2044605dbf75370912e81127c679af51
Harsh Kumar Pandey <coding.harshp@gmail.com>
2021-02-01 02:03:56 +0530

    Reusable Toggle Component

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import { AboutSec } from "../style";
+import Toggle from "./Toggle";
+import { AnimateSharedLayout } from "framer-motion";
 
 const Faq = () => {
   return (
@@ -8,50 +10,44 @@ const Faq = () => {
         <h2>
           Any Questions <span>FAQ</span>
         </h2>
-        <div className="question">
-          <h4>How do I start ?</h4>
-          <div className="answer">
-            <p>Lorem ipsum dolor sit amet.</p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
-              atque maxime natus.
-            </p>
-          </div>
-          <div className="faq-line" />
-        </div>
-        <div className="question">
-          <h4>Daily Schedule</h4>
-          <div className="answer">
-            <p>Lorem ipsum dolor sit amet.</p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
-              atque maxime natus.
-            </p>
-          </div>
-          <div className="faq-line" />
-        </div>
-        <div className="question">
-          <h4>Different Payment Methods</h4>
-          <div className="answer">
-            <p>Lorem ipsum dolor sit amet.</p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
-              atque maxime natus.
-            </p>
-          </div>
-          <div className="faq-line" />
-        </div>
-        <div className="question">
-          <h4>What product do you offer ?</h4>
-          <div className="answer">
-            <p>Lorem ipsum dolor sit amet.</p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
-              atque maxime natus.
-            </p>
-          </div>
-          <div className="faq-line" />
-        </div>
+        <AnimateSharedLayout>
+          <Toggle title="How do I start ?">
+            <div className="answer">
+              <p>Lorem ipsum dolor sit amet.</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
+                atque maxime natus.
+              </p>
+            </div>
+          </Toggle>
+          <Toggle title="Daily Schedule">
+            <div className="answer">
+              <p>Lorem ipsum dolor sit amet.</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
+                atque maxime natus.
+              </p>
+            </div>
+          </Toggle>
+          <Toggle title="Different Payment Methods">
+            <div className="answer">
+              <p>Lorem ipsum dolor sit amet.</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
+                atque maxime natus.
+              </p>
+            </div>
+          </Toggle>
+          <Toggle title="What product do you offer ?">
+            <div className="answer">
+              <p>Lorem ipsum dolor sit amet.</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
+                atque maxime natus.
+              </p>
+            </div>
+          </Toggle>
+        </AnimateSharedLayout>
       </FAQ>
     </>
   );
