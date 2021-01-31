f821b9926be2d0c72b53cc30718f0a3694a7ef0e
Harsh Kumar Pandey <coding.harshp@gmail.com>
2021-01-31 12:29:03 +0530

    Animating AboutUs

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import Nav from "./components/Nav";
 import AboutUs from "./pages/AboutUs";
 import OurWork from "./pages/OurWork";
 import ContactUs from "./pages/ContactUs";
+import MovieDetail from "./pages/MovieDetail";
 
 import GlobalStyle from "./components/GlobalStyle";
 
 import { Switch, Route, useLocation } from "react-router-dom";
-import MovieDetail from "./pages/MovieDetail";
-
 import { AnimatePresence } from "framer-motion";
+
 function App() {
   const location = useLocation();
   console.log(location);
--- src/components/About.js
+++ src/components/About.js
@@ -2,32 +2,32 @@ import home1 from "../img/home1.png";
 import styled from "styled-components";
 import { AboutSec, Description, Image, Hide } from "../style";
 import { motion } from "framer-motion";
-
+import { titleAnim, fade, photoAnim } from "../animation";
 const About = () => {
   return (
     <AboutSec>
       <Description>
         <motion.div>
           <Hide>
-            <motion.h2>We work to make</motion.h2>
+            <motion.h2 variants={titleAnim}>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>
+            <motion.h2 variants={titleAnim}>
               your <span>dreams</span> come
             </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>true.</motion.h2>
+            <motion.h2 variants={titleAnim}>true.</motion.h2>
           </Hide>
         </motion.div>
-        <p>
+        <motion.p variants={fade}>
           Contact us for any photography or videography ideas that you have. we
           are professionals with amazing skills
-        </p>
-        <button>Contact Us</button>
+        </motion.p>
+        <motion.button variants={fade}>Contact Us</motion.button>
       </Description>
       <Image>
-        <img src={home1} alt="guy with a camera" />
+        <motion.img variants={photoAnim} src={home1} alt="guy with a camera" />
       </Image>
     </AboutSec>
   );
